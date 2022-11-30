4365692d4a966732e8b06739bdb409f1a46b3dcc
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-11-30 22:56:31 +0700

    test
    da

--- Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/model/Message.java
+++ Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/model/Message.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package co.g2academy.kelarin.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 /**
  *
  * @author Ichsan S
  */
+@Entity
+@Table(name="t_message")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    @ManyToOne(optional=false)
+    @JoinColumn(name="user_id",nullable=false)
+    private User user;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
     
 }
