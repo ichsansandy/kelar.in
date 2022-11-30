569626cd190ad2d92354d90e15da61eac918e45b
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-11-30 23:41:15 +0700

    Create Push notification

---
+++ Kelar.in - Push Notification/src/main/java/co/g2academy/kelarin/model/PushNotification.java
@@ -0,0 +1,15 @@
+
+package co.g2academy.kelarin.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author Ichsan S
+ */
+@Entity
+@Table(name="t_push_notification")
+public class PushNotification {
+    
+}
