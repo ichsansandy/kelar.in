92d01742ca69f2ae7e6d181071f69a9976944daa
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-11-30 23:12:18 +0700

    model message

--- Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/model/Message.java
+++ Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/model/Message.java
@@ -1,10 +1,12 @@
 package co.g2academy.kelarin.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -18,9 +20,18 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
+    
     @ManyToOne(optional=false)
-    @JoinColumn(name="user_id",nullable=false)
-    private User user;
+    @JoinColumn(name="sender_id",nullable=false)
+    private User sender;
+    
+    @ManyToOne(optional=false)
+    @JoinColumn(name="receiver_id",nullable=false)
+    private User receiver;
+    
+    @Column(nullable=false)
+    @Lob
+    private String message;
 
     public Integer getId() {
         return Id;
@@ -30,13 +41,7 @@ public class Message {
         this.Id = Id;
     }
 
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
+    
     
     
     
