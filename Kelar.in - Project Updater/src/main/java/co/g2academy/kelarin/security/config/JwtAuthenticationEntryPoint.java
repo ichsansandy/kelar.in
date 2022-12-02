aee6ec46eb742d213d33206411a7d7411ad75faa
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-02 20:58:52 +0700

    Security @Component Missing

--- Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/security/config/JwtAuthenticationEntryPoint.java
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/security/config/JwtAuthenticationEntryPoint.java
@@ -6,11 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author Ichsan S
  */
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     
     @Override
--- Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/security/filter/JwtRequestFilter.java
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/security/filter/JwtRequestFilter.java
@@ -13,12 +13,14 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
  *
  * @author Ichsan S
  */
+@Component
 public class JwtRequestFilter extends OncePerRequestFilter {
      @Autowired
     private JwtUserDetailService userService;
