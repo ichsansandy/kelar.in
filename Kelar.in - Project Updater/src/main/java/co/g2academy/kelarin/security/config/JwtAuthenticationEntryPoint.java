c81cf8bb9a8373e3e225447df3ca7c0ac8488fad
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-02 20:51:24 +0700

    Update JwtAuthenticationEntryPoint.java

--- Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/security/config/JwtAuthenticationEntryPoint.java
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/security/config/JwtAuthenticationEntryPoint.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package co.g2academy.kelarin.security.config;
 
 import java.io.IOException;
