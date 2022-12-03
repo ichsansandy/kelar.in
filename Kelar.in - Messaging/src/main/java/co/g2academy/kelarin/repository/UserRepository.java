8c93ff111a55ba5ce35158814a94f023f7c0bf8c
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-03 13:47:30 +0700

    Create UserRepository.java

---
+++ Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/repository/UserRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package co.g2academy.kelarin.repository;
+
+import co.g2academy.kelarin.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Ichsan S
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    
+}
