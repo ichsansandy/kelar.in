e97ee0e5c1f282d48ca3b4af93076f41c14ef228
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-03 14:57:35 +0700

    Update Message.java

--- Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/model/Message.java
+++ Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/model/Message.java
@@ -1,5 +1,6 @@
 package co.g2academy.kelarin.model;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,34 +10,40 @@ import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
  * @author Ichsan S
  */
 @Entity
-@Table(name="t_message")
+@Table(name = "t_message")
 public class Message {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
-    
-    @ManyToOne(optional=false)
-    @JoinColumn(name="sender_id",nullable=false)
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "sender_id", nullable = false)
     private User sender;
-    
-    @ManyToOne(optional=false)
-    @JoinColumn(name="receiver_id",nullable=false)
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
-    
-    @Column(nullable=false)
+
+    @Column(nullable = false)
     @Lob
     private String message;
-    
+
     @ManyToOne(optional = false)
     @JoinColumn(name = "message_room_id", nullable = false)
     private MessageRoom messageRoom;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
 
     public Integer getId() {
         return Id;
@@ -77,5 +84,13 @@ public class Message {
     public void setMessageRoom(MessageRoom messageRoom) {
         this.messageRoom = messageRoom;
     }
-    
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
 }
