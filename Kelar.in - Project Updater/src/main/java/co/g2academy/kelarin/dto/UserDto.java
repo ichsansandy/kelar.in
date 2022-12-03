ac7c622dedf9345e10a9783d6ecb298806742c95
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-03 13:00:45 +0700

    Create UserDto.java

---
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/dto/UserDto.java
@@ -0,0 +1,29 @@
+package co.g2academy.kelarin.dto;
+
+/**
+ *
+ * @author Ichsan S
+ */
+public class UserDto {
+    
+    private String name;
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
