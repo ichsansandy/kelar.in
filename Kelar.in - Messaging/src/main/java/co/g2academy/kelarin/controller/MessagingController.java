3b042d48ed4c9f67083503d413a45fea861583da
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-03 13:51:18 +0700

    Update MessagingController.java

--- Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/controller/MessagingController.java
+++ Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/controller/MessagingController.java
@@ -1,5 +1,12 @@
 package co.g2academy.kelarin.controller;
 
+import co.g2academy.kelarin.repository.MessageRepository;
+import co.g2academy.kelarin.repository.MessageRoomRepository;
+import co.g2academy.kelarin.repository.UserRepository;
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MessagingController {
     
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private MessageRepository messageRepo;
+    @Autowired
+    private MessageRoomRepository roomRepo;
     
+    
+    @PostMapping("/message-room")
+    public ResponseEntity createRoom(Principal principal){
+        return ResponseEntity.ok().body("OK");
+    }
 }
