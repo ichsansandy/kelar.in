f55dc78142e6f00f6f94f4ea5e3f14562da63a7f
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-03 14:02:25 +0700

    Update MessagingController.java

--- Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/controller/MessagingController.java
+++ Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/controller/MessagingController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.data.redis.serializer.RedisSerializationContext.java;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,9 +30,13 @@ public class MessagingController {
     
     
     @PostMapping("/message-room")
-    public ResponseEntity createRoom(Principal principal){
+    public ResponseEntity createRoom(@RequestBody User user2,Principal principal){
         User loggedInUser = userRepo.findUserByUsername(principal.getName());
+        // get All room list by user1 and user 2
         
+        // check if the user already created room both in user 1 and 2
+        // if null create the room
+        // else found getRoom by id
         return ResponseEntity.ok().body("OK");
     }
 }
