cedbc4a9b1d44abad69a7a1c1158ee7be3cf8f6b
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-03 13:41:44 +0700

    Create MessagingController.java

---
+++ Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/controller/MessagingController.java
@@ -0,0 +1,15 @@
+package co.g2academy.kelarin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Ichsan S
+ */
+@RestController
+@RequestMapping("/api")
+public class MessagingController {
+    
+    
+}
