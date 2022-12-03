371fc852c543493c87338d2a498d7db76a92fcd7
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-03 13:17:22 +0700

    Create MessagePublisherService.java

---
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/service/MessagePublisherService.java
@@ -0,0 +1,21 @@
+package co.g2academy.kelarin.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Ichsan S
+ */
+@Component
+public class MessagePublisherService {
+    @Autowired
+    @Qualifier("redisPubSubTemplate")
+    private RedisTemplate<String , String> template;
+    
+    public void publish (String message){
+        template.convertAndSend("userCreationPubSub", message);
+    }
+}
