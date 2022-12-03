94a8d710bd3b1bd3b81c7d5c32ef1a2f073b435a
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-03 14:16:49 +0700

    Update MessagingController.java

--- Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/controller/MessagingController.java
+++ Kelar.in - Messaging/src/main/java/co/g2academy/kelarin/controller/MessagingController.java
@@ -1,10 +1,12 @@
 package co.g2academy.kelarin.controller;
 
+import co.g2academy.kelarin.model.MessageRoom;
 import co.g2academy.kelarin.model.User;
 import co.g2academy.kelarin.repository.MessageRepository;
 import co.g2academy.kelarin.repository.MessageRoomRepository;
 import co.g2academy.kelarin.repository.UserRepository;
 import java.security.Principal;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.data.redis.serializer.RedisSerializationContext.java;
 import org.springframework.http.ResponseEntity;
@@ -20,23 +22,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class MessagingController {
-    
+
     @Autowired
     private UserRepository userRepo;
     @Autowired
     private MessageRepository messageRepo;
     @Autowired
-    private MessageRoomRepository roomRepo;
-    
-    
+    private MessageRoomRepository messageRoomRepo;
+
     @PostMapping("/message-room")
-    public ResponseEntity createRoom(@RequestBody User user2,Principal principal){
+    public ResponseEntity createRoom(@RequestBody User user2, Principal principal) {
         User loggedInUser = userRepo.findUserByUsername(principal.getName());
         // get All room list by user1 and user 2
-        
+        List<MessageRoom> rooms = messageRoomRepo.findAll();
         // check if the user already created room both in user 1 and 2
-        // if null create the room
-        // else found getRoom by id
-        return ResponseEntity.ok().body("OK");
+        for (MessageRoom room : rooms) {
+            if ((room.getUser1().getName().equals(loggedInUser.getName())
+                    || room.getUser2().getName().equals(loggedInUser.getName()))
+                    && (room.getUser1().getName().equals(user2.getName())
+                    || room.getUser2().getName().equals(user2.getName()))) {
+                // else found getRoom by id
+            } else {
+                // if null create the room
+                MessageRoom newRoom = new MessageRoom();
+                newRoom.setUser1(loggedInUser);
+                newRoom.setUser2(user2);
+                messageRoomRepo.save(newRoom);
+                return ResponseEntity.ok().body("OK");
+            }
+        }
+        return ResponseEntity.badRequest().body("Something wrong with room validation");
     }
 }
