1b3e2c5ee8b1161d7741b69a4f1f08a0b62d8e5e
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-04 17:48:33 +0700

    Update TaskLogToPushNotificationDto.java

--- Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/dto/TaskLogToPushNotificationDto.java
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/dto/TaskLogToPushNotificationDto.java
@@ -1,9 +1,48 @@
 package co.g2academy.kelarin.dto;
 
+import co.g2academy.kelarin.model.TaskLog;
+import java.util.Date;
+import static org.springframework.data.redis.serializer.RedisSerializationContext.java;
+
 /**
  *
  * @author Ichsan S
  */
 public class TaskLogToPushNotificationDto {
+    
+    private String pushNotificationType;
+    private String description;
+    private Date date;
+
+   public TaskLogToPushNotificationDto(TaskLog tasklog){
+       pushNotificationType = tasklog.getLogType();
+       description = tasklog.getLogDescription();
+       date = new Date();
+   }
+    
+    public String getPushNotificationType() {
+        return pushNotificationType;
+    }
+
+    public void setPushNotificationType(String pushNotificationType) {
+        this.pushNotificationType = pushNotificationType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
     
 }
