e2c67369f03c382a5708f82ba65754919bd3f3eb
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-04 17:17:10 +0700

    Update ProjectRepository.java

--- Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/repository/ProjectRepository.java
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package co.g2academy.kelarin.repository;
 
 import co.g2academy.kelarin.model.Project;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer>{
-
+    
+    public List<Project> findProjectByUser(User user);
 }
