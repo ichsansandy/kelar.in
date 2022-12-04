010f7869adc78c047a254f05764b506bd5495071
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-04 11:21:56 +0700

    Update PushNotification.java

--- Kelar.in - Push Notification/src/main/java/co/g2academy/kelarin/model/PushNotification.java
+++ Kelar.in - Push Notification/src/main/java/co/g2academy/kelarin/model/PushNotification.java
@@ -1,8 +1,15 @@
 
 package co.g2academy.kelarin.model;
 
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
@@ -11,5 +18,52 @@ import javax.persistence.Table;
 @Entity
 @Table(name="t_push_notification")
 public class PushNotification {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(nullable=false)
+    private String pushNotificationType;
+    
+    @Column(nullable=false)
+    private String description;
+    
+    @Column(nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPushNotificationType() {
+        return pushNotificationType;
+    }
+
+    public void setPushNotificationType(String pushNotificationType) {
+        this.pushNotificationType = pushNotificationType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
     
 }
