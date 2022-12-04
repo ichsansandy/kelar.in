b7f9d9517e7a7a907becbe21f20ef3c8406a62d9
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-04 16:26:37 +0700

    Create PushNotificationMessageListenerService.java

---
+++ Kelar.in - Push Notification/src/main/java/co/g2academy/kelarin/service/PushNotificationMessageListenerService.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.g2academy.kelarin.service;
+
+import co.g2academy.kelarin.model.PushNotification;
+import co.g2academy.kelarin.model.User;
+import co.g2academy.kelarin.repository.PushNotificationRepository;
+import co.g2academy.kelarin.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.SortParameters.Order;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Ichsan S
+ */
+@Service
+public class PushNotificationMessageListenerService implements MessageListener{
+    @Autowired
+    @Qualifier("redisPubSubTemplate")
+    private RedisTemplate<String,String> redisTemplate;
+    @Autowired
+    private PushNotificationRepository repository;
+    private ObjectMapper mapper = new JsonMapper();
+    
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            String json = new String (message.getBody());
+            PushNotification pushNotification = mapper.readValue(json, new TypeReference<PushNotification>(){});
+            repository.save(pushNotification);
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
