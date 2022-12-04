12d50f8e0f2072c59dd4b20b1290ad2523e5db36
Ichsan Sandypratama <ichsan2668@gmail.com>
2022-12-05 06:29:51 +0700

    TaskLog changes to project FK

--- Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/repository/TaskLogRepository.java
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/repository/TaskLogRepository.java
@@ -1,6 +1,6 @@
 package co.g2academy.kelarin.repository;
 
-import co.g2academy.kelarin.model.Task;
+import co.g2academy.kelarin.model.Project;
 import co.g2academy.kelarin.model.TaskLog;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface TaskLogRepository extends JpaRepository<TaskLog, Integer>{
+    public List<TaskLog> findTaskLogByProject(Project project);
 }
