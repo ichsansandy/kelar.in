65897de8776f266a07d4cf6b95af7b5fa81a6b8d
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-04 14:43:04 +0700

    Update UserRepository.java

--- Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/repository/UserRepository.java
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/repository/UserRepository.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Integer>{
     
     public User findUserByUsername(String username);
+    public User findUserByName(String name);
 }
