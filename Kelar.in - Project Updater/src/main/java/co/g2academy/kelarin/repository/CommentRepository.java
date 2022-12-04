424e8199e913a78f3a41f5105ead31a940df071f
Six-Se7en <108980933+Six-Se7en@users.noreply.github.com>
2022-12-04 17:06:58 +0700

    Update CommentRepository.java

--- Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/repository/CommentRepository.java
+++ Kelar.in - Project Updater/src/main/java/co/g2academy/kelarin/repository/CommentRepository.java
@@ -1,9 +1,11 @@
 package co.g2academy.kelarin.repository;
 
 import co.g2academy.kelarin.model.Comment;
+import co.g2academy.kelarin.model.Project;
 import co.g2academy.kelarin.model.Task;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import static org.springframework.data.redis.serializer.RedisSerializationContext.java;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -13,6 +15,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer>{
     
-    public List<Comment> findCommentByTask(Task task);
+    public List<Comment> findCommentByProject(Project project);
 
 }
