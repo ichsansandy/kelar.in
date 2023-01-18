84a1e4aa5187226b81809d92347263d7170bdf9e
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-01-18 18:29:16 +0700

    add home page style height

--- kelar-in-web-app/src/component/AssignProject.js
+++ kelar-in-web-app/src/component/AssignProject.js
@@ -13,10 +13,10 @@ function AssignProject() {
       <div className=" w-fit py-2 px-4 text-3xl mr-6 border-b-4 border-b-third-color ">Assign Project to You</div>
       <div className="flex ">
         <ul className=
-        // {
-            "flex items-center w-full snap-x overflow-x-auto p-4 mx-1 h-72 " 
-        // + (projects.length <= 3) ? " h-64 " : " h-72"
-        // }
+        {
+            "flex items-center w-full snap-x overflow-x-auto p-4 mx-1 " 
+        + (projects.length >= 3 ? "h-64 " : "h-72")
+        }
         >
           {projects.map((project) => (
             <ProjectCard project={project} />
